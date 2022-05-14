35157c6bc73b28e39b3c79bf4bb79c0e4e580605
newjk <scolpionsjk@naver.com>
2022-05-14 14:37:33 +0900

    Initial commit

---
+++ src/main/java/NERDPRESS/NERDPRESS/NerdpressApplication.java
@@ -0,0 +1,13 @@
+package NERDPRESS.NERDPRESS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NERDPRESS/NERDPRESS/NerdpressApplicationTests.java
@@ -0,0 +1,13 @@
+package NERDPRESS.NERDPRESS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
