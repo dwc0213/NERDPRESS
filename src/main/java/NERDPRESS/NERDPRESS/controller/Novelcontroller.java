9529a41c1921852d94e843b82a963b3192ecc69d
최범식 <jin5myung@naver.com>
2022-05-19 21:02:58 +0900

    index.html + 상단 바 조금 수정하고, a 링크를 리다이렉트 했습니다(컨트롤러로 빠지게) 더불어 컨트롤러를 몇몇개 만들어서 매핑하기 시작했습니다. 컨트롤러 위마다 주석 달고 + index.html 에 링크 달아주는거 + 리다이렉트되는 html 파일을 달았습니다

--- src/main/java/NERDPRESS/NERDPRESS/controller/Novelcontroller.java
+++ src/main/java/NERDPRESS/NERDPRESS/controller/Novelcontroller.java
@@ -1,4 +1,63 @@
 package NERDPRESS.NERDPRESS.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class Novelcontroller {
+
+    // 메인페이지로 이동하는 컨트롤러 mainpage - index.html
+    @GetMapping("mainpage")
+    public String mainPage(){
+        return "redirect:/";
+    }
+
+    // 작가리스트 writerlist - WriterList.html
+    @GetMapping("writerlist")
+    public String writerList(){
+        return "content/WriterList";
+    }
+
+    // 로그인 페이지 login - login.html
+    @GetMapping("login")
+    public String login(){
+        return "login/login";
+    }
+
+    // 회원가입 signup - signUp.html
+    @GetMapping("signup")
+    public String signup(){
+        return "login/signUp";
+    }
+
+    // 웹소설 검색 searchnovel - SearchNovel.html
+    @GetMapping("searchnovel")
+    public String searchNovel(){
+        return "content/SearchNovel";
+    }
+
+    // 월간베스트 monthbest - monthBest.html
+    @GetMapping("monthbest")
+    public String monthBest(){
+        return "content/monthBest";
+    }
+
+    // 최신 웹소설 목록 newest - newest.html
+    @GetMapping("newest")
+    public String newest(){
+        return "content/newest";
+    }
+
+    // 작가 정보 writerinfro - WriterInformation.html
+    @GetMapping("writerinfo")
+    public String writerInfo(){
+        return "content/WriterInformation";
+    }
+
+    // 작품 회차 리스트 contentlist - contentList.html
+    @GetMapping("contentlist")
+    public String contentList(){
+        return "content/contestList";
+    }
+
 }
