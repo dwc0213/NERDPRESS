0f57fea59a11d1c9c27c1ee405eb729150e16c55
최범식 <jin5myung@naver.com>
2022-05-21 14:32:18 +0900

    // 최범식 admin 및 novel jdbc template 설정했읍니다

--- src/main/java/NERDPRESS/NERDPRESS/domain/Novel.java
+++ src/main/java/NERDPRESS/NERDPRESS/domain/Novel.java
@@ -4,7 +4,48 @@ public class Novel {
     private int id;
     private int date;
     private String title;
-    private int genre;
+    private String genre;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     /*
     * 1: 로맨스
     * 2: 판타지
--- src/main/java/NERDPRESS/NERDPRESS/repository/NovelRepositoryInterface.java
+++ src/main/java/NERDPRESS/NERDPRESS/repository/NovelRepositoryInterface.java
@@ -1,4 +1,18 @@
 package NERDPRESS.NERDPRESS.repository;
 
+import NERDPRESS.NERDPRESS.domain.Novel;
+
+import java.util.List;
+
 public interface NovelRepositoryInterface {
+
+    // 소설을 저장
+    void saveNovel(Novel n);
+
+    // 소설찾기
+    Novel findNovel(String novel);
+
+    // 소설 20개 목록
+    List<Novel> topTwenty();
+
 }
